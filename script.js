91b3a0b59038e6f70a6d1c8d661713b691d624a1
Iman Anooshehpour <imananoosheh@gmail.com>
2022-11-03 17:09:28 -0700

    added game logic

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ function play(cell) {
     }else{
         cell.classList.toggle('o')
     }
+
+    //checking if we have a winner
+    winnerCheck()
     // toggles the turn
     turn = turn === 'x' ? 'o' : 'x'
 }
@@ -26,4 +29,55 @@ function reset(){
     cellList.forEach((cell)=>{
         cell.className = 'cell'
     })
+}
+
+function winnerCheck(){
+    let grid = []
+    cellList.forEach((cell)=>{
+        let mark
+        if(cell.classList.contains('x')){
+            mark = 'x'
+        }else if(cell.classList.contains('o')){
+            mark = 'o'
+        }else{
+            mark = null
+        }
+        grid.push(mark)
+    })
+    
+    //winner logic
+    //vertical cells check
+    if(are3EqualAndNotNull(grid[0],grid[3],grid[6])){
+        console.log(`The ${grid[0]} is the WINNER!`)
+    }
+    if(are3EqualAndNotNull(grid[1],grid[4],grid[7])){
+        console.log(`The ${grid[1]} is the WINNER!`)
+    }
+    if(are3EqualAndNotNull(grid[2],grid[5],grid[8])){
+        console.log(`The ${grid[2]} is the WINNER!`)
+    }
+
+    //horizontal cells check
+    if(are3EqualAndNotNull(grid[0],grid[1],grid[2])){
+        console.log(`The ${grid[0]} is the WINNER!`)
+    }
+    if(are3EqualAndNotNull(grid[3],grid[4],grid[5])){
+        console.log(`The ${grid[3]} is the WINNER!`)
+    }
+    if(are3EqualAndNotNull(grid[6],grid[7],grid[8])){
+        console.log(`The ${grid[6]} is the WINNER!`)
+    }
+
+    //diagonal cells checks
+    if(are3EqualAndNotNull(grid[0],grid[4],grid[8])){
+        console.log(`The ${grid[0]} is the WINNER!`)
+    }
+    if(are3EqualAndNotNull(grid[2],grid[4],grid[6])){
+        console.log(`The ${grid[2]} is the WINNER!`)
+    }
+    // TODO: should be stopping the game here 
+}
+
+function are3EqualAndNotNull(a,b,c){
+    return (a===b && b===c && a!==null)
 }
